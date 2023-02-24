08a0c9b86a6db621e4ff87ccbed278e174c57a12
Usirusen Ikon <lexicon048@gmail.com>
2023-02-24 13:26:32 +0100

    Add upload function for icon column

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,7 @@ class Group < ApplicationRecord
   belongs_to :user
   has_many :group_accounts
   has_many :accounts, through: :group_accounts
+  has_one_attached :icon
 
   validates :icon, :name, presence: true
 end
